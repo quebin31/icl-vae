15ea2f7d283a8fca6d7008becf9c47c008c9795a
quebin31 <quebin31@gmail.com>
2020-09-14 16:48:02 +0000

    Draft for VGG module

--- vgg.py
+++ vgg.py
@@ -1,6 +1,5 @@
+import torch
 from torch import nn
-
-vgg19_dict = 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth'
 
 
 class VGG19(nn.Module):
@@ -13,5 +12,35 @@ class VGG19(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1)
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
+
+    def forward(self, x):
+        with torch.no_grad():
+            features = self.features(x)
+            # start from dim-1, since dim-0 is for batch
+            return torch.flatten(features, start_dim=1)
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    test = torch.randn(1, 3, 32, 32, device=device)
+    vgg = VGG19().to(device)
+
+    out = vgg(test)
+    print(out.shape)
