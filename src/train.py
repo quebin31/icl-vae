66b1bcd13cee0b96175bf4c5f728fbb2ceeff7b5
quebin31 <quebin31@gmail.com>
2020-10-19 15:23:14 +0000

    Change the way tasks and datasets are handled. Now we receive the dataset and split it inside the train function to create the proper dataloaders based on the subset of the dataset. Also added class consolidation after training, this calculate the class means and store them on the model.

--- src/train.py
+++ src/train.py
@@ -1,58 +1,86 @@
+import os
+import torch
+import wandb
+
 from mas import LocalSgd, OmegaSgd, compute_omega_grads_norm
 from modules.vicl import Vicl
-from torch.utils.data import DataLoader
-from utils import model_criterion
+from torch.utils.data import DataLoader, Dataset, Subset
+from torch.optim.lr_scheduler import ExponentialLR
+from utils import model_criterion, split_classes_in_tasks
 
 
-def train(model: Vicl, task: int, epochs: int, dataloader_train: DataLoader, dataloader_test: DataLoader, train_size: int, test_size: int, lr=0.001, reg_lambda=0.01):
+def create_data_loaders(dataset_train: Dataset, dataset_test: Dataset, task: int, batch_size: int, num_workers: int = 6):
+    train_tasks = split_classes_in_tasks(dataset_train)
+    test_tasks = split_classes_in_tasks(dataset_test)
+
+    assert len(train_tasks) == len(test_tasks)
+
+    print(f"Total of tasks: {len(train_tasks)}")
+    train_subset = Subset(dataset_train, train_tasks[task])
+    test_subset = Subset(dataset_test, train_tasks[task])
+
+    dataloader_train = DataLoader(
+        train_subset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    dataloader_test = DataLoader(
+        test_subset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    return dataloader_train, dataloader_test
+
+
+def train(model: Vicl, dataset_train: Dataset, dataset_test: Dataset, task: int, epochs: int, batch_size: int, lr=0.000003, reg_lambda=0.01, log_interval=100):
     # Init regularizer params (omega values) according to the task number
     if task == 0:
         model._init_reg_params_first_task()
     else:
         model._init_reg_params_subseq_tasks()
 
-    store_path = os.path.join(os.getcwd(), "models", f"Task{task}")
-    model_path = os.path.join(os.getcwd(), "models")
-
-    if task == 0 and not os.path.isdir(model_path):
-        os.mkdir(model_path)
+    # Get the device we are going to use
+    device = model.device()
 
     # We're training our model
     model.vae.train()
-    device = model.device()
-    model_optimizer = LocalSgd(model.parameters(), reg_lambda, lr=lr)
+    model_optimizer = LocalSgd(model.vae.parameters(), reg_lambda, lr=lr)
+    moptim_scheduler = ExponentialLR(model_optimizer, gamma=0.7)
+
+    # Create the data loaders
+    dataloader_train, dataloader_test = create_data_loaders(
+        dataset_train, dataset_test, task=task, batch_size=batch_size)
+    num_batches = len(dataloader_train)
 
     # Start training the model
     for epoch in range(0, epochs):
-        print(f"Epoch {epoch + 1}/{epochs}: ", end="")
-
         total_loss = 0.0
-        total_corrects = 0.0
 
-        for batch in dataloader_train:
+        print(f"Epoch {epoch + 1}/{epochs}:")
+        for batch_idx, batch in enumerate(dataloader_train):
             data, labels = batch
             data = data.to(device)
             labels = labels.to(device)
 
             model_optimizer.zero_grad()
-            (x_mu, x_logvar), z_mu, z_logvar = model(data)
+
+            output = model(data)
+            x_features = output["features"]
+            x_mu, x_logvar = output["x_mu"], output["x_logvar"]
+            z_mu, z_logvar = output["z_mu"], output["z_logvar"]
 
             loss = model_criterion(
-                data, labels, x_mu, x_logvar, z_mu, z_logvar)
+                x_features, labels, x_mu, x_logvar, z_mu, z_logvar)
             loss.backward()
 
             model_optimizer.step(model.reg_params)
 
-            prediction = model.predict(data, z_mu=z_mu, z_logvar=z_logvar)
-            prediction = torch.tensor(prediction).to(device)
+            total_loss += loss.cpu().item()
+            mean_loss = total_loss / (batch_idx + 1)
 
-            total_loss += loss.item()
-            total_corrects += torch.sum(prediction == labels)
+            print(
+                f"\r:: ({batch_idx + 1}/{num_batches}) Loss: {mean_loss:.4f}", end="")
 
-        epoch_loss = total_loss / train_size
-        epoch_accuracy = total_corrects / train_size
+            if batch_idx % log_interval == 0:
+                wandb.log({"Loss": mean_loss})
 
-        print(f"Loss: {epoch_loss:.4f}, Acc.: {epoch_accuracy:.4f}")
+        print()
+        moptim_scheduler.step()
 
     # After training the model for this task update the omega values
     print(f"Updating omega values for this task")
@@ -63,8 +91,42 @@ def train(model: Vicl, task: int, epochs: int, dataloader_train: DataLoader, dat
     if task > 0:
         model._consolidate_reg_params()
 
-    # Save model for this task
-    save_path = os.path.join(os.getcwd(), "..", "models", f"vicl_task_{task}.pth")
-    model.save(save_path)
+    # Actually "learn" the new class(es)
+    model.eval()
+    label_total = {}
+    for batch in dataloader_train:
+        data, labels = batch
+        data = data.to(device)
+        labels = labels.to(device)
+
+        _, z_mu, z_logvar = model(data)
+
+        # Sum the "mu" and "logvar" values, also count the total for each label
+        for i in range(0, labels.size(0)):
+            label = labels[i].cpu().item()
+
+            model.class_idents.setdefault(label, {}).setdefault(
+                "mu", torch.zeros(z_mu.size(1))).add_(z_mu[i])
+
+            model.class_idents.setdefault(label, {}).setdefault(
+                "logvar", torch.zeros(z_logvar.size(1))).add_(z_logvar[i])
+
+            label_total[label] = label_total.get(label, 0) + 1
+
+    # Divide by the total of each label
+    for label, total in label_total.items():
+        model.class_idents[label] /= total
+
+    # Create dir to save models
+    model_path = os.path.join(os.getcwd(), "models")
+    if task == 0 and not os.path.isdir(model_path):
+        os.mkdir(model_path)
+
+    # Save the model locally and to wandb
+    local_path = os.path.join(model_path, f"vicl_task_{task}.pt")
+    wandb_path = os.path.join(wandb.run.dir, f"vicl_task_{task}.pt")
+
+    model.save(local_path)
+    model.save(wandb_path)
 
     return model
