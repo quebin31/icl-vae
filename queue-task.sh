9d9f1831832d32a89436e8effd9d6c44ffac8363
quebin31 <quebin31@gmail.com>
2020-11-05 20:58:46 +0000

    feat: reduce script

--- queue-task.sh
+++ queue-task.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-
-task="$1"
-runid="$2"
-seed="$3"
-batch="16"
-lr="0.000004" 
-rlambda="0.01"
-epochs="100"
-decay="0.8"
-   
+  
 pueue add -- python3 src/main.py \
-    --batch "${batch}" \
-    --lr "${lr}" \
-    --rlambda "${rlambda}" \
-    --epochs "${epochs}" \
-    --seed "${seed}" \
-    --task "${task}" \
-    --run-id "${runid}" \
-    --decay "${decay}" \
+    --config "$1" \
+    --task "$2" \
+    --run-id "$3" \
     --train --test 
