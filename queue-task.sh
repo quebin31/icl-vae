4e984884e0ac6090329fec783c050226258bb84f
quebin31 <quebin31@gmail.com>
2020-11-05 17:58:43 +0000

    feat: don't use string for command

--- queue-task.sh
+++ queue-task.sh
@@ -4,18 +4,18 @@ task="$1"
 runid="$2"
 seed="$3"
 batch="16"
-lr="0.000003"
+lr="0.000004" 
 rlambda="0.01"
-epochs="10"
-
-cmd="python3 src/main.py \
-    --batch ${batch} \
-    --lr ${lr} \
-    --rlambda ${rlambda} \
-    --epochs ${epochs}\
-    --seed ${seed} \
-    --task ${task} \
-    --run-id ${runid} \
-    --train --test"
-    
-pueue add -- "${cmd}"
+epochs="100"
+decay="0.8"
+   
+pueue add -- python3 src/main.py \
+    --batch "${batch}" \
+    --lr "${lr}" \
+    --rlambda "${rlambda}" \
+    --epochs "${epochs}" \
+    --seed "${seed}" \
+    --task "${task}" \
+    --run-id "${runid}" \
+    --decay "${decay}" \
+    --train --test 
