7976d76202f24c149e92646407dd8b801229cc45
quebin31 <quebin31@gmail.com>
2020-11-13 16:41:24 -0500

    feat: test with base, new and all subset

--- src/test.py
+++ src/test.py
@@ -1,22 +1,43 @@
 import wandb
 
 from modules.vicl import Vicl
-from torch.utils.data import Dataset
-from utils import create_data_loader
+from torch.utils.data import Dataset, DataLoader
+from utils import split_classes_in_tasks, create_subset
 from halo import Halo
+from typing import List, Optional
 
 
 def test(model: Vicl, dataset: Dataset, task: int, batch_size: int):
-    model.eval()
+    tasks_indices = split_classes_in_tasks(dataset)
+
+    base_subset = create_subset(
+        dataset, task=0, tasks_indices=tasks_indices, accumulate=False)
+    test_with_subset('(Base)', model, base_subset, batch_size)
+
+    new_subset = create_subset(
+        dataset, task=task, tasks_indices=tasks_indices, accumulate=False)
+    test_with_subset('(New)', model, new_subset, batch_size)
+
+    all_subset = create_subset(
+        dataset, task=task, tasks_indices=tasks_indices, accumulate=True)
+    test_with_subset('(All)', model, all_subset, batch_size)
+
+    return model
+
+
+def test_with_subset(metric: str, model: Vicl, dataset: Dataset, batch_size: int):
     device = model.device()
 
-    dataloader = create_data_loader(dataset, task=task, batch_size=batch_size)
+    dataloader = DataLoader(dataset, batch_size=batch_size,
+                            shuffle=True, num_workers=6)
     num_batches = len(dataloader)
 
     label_corrects = {}
     label_totals = {}
 
-    prefix = 'Testing accuracy'
+    model.eval()
+
+    prefix = f'{metric} Testing accuracy'
     halo = Halo(text=prefix, spinner='dots').start()
     for batch_idx, batch in enumerate(dataloader):
         halo.text = f'{prefix} ({batch_idx + 1}/{num_batches})'
@@ -41,11 +62,14 @@ def test(model: Vicl, dataset: Dataset, task: int, batch_size: int):
     assert len(label_corrects) == len(label_totals)
 
     total_accuracy = 0
+    acc = {}
     for label, total in label_totals.items():
         accuracy = label_corrects[label] / total
         total_accuracy += accuracy
-        wandb.log({f'Acc. Label {label} Task {task}': accuracy})
+        acc[f'{metric} Acc. Label {label} Task {task}'] = accuracy
 
-    wandb.log({f'Mean Acc. Task {task}': total_accuracy / len(label_totals)})
+    acc[f'{metric} Mean Acc. Task {task}'] = total_accuracy / len(label_totals)
+    wandb.log(acc)
+
     halo.succeed()
     return model
