8f7ddf57fbc2d1d0b38c413b7de7cabc3d4ebdad
quebin31 <quebin31@gmail.com>
2020-12-11 11:08:21 -0500

    Allow extra args on scripts

--- base.sh
+++ base.sh
@@ -3,4 +3,5 @@
 set -e
 
 config="$1"
-pipenv run python src/main.py --task=0 --config="$config" --train --test
+shift
+pipenv run python src/main.py --task=0 --config="$config" --train --test "$@"
--- incremental.sh
+++ incremental.sh
@@ -4,8 +4,9 @@ set -e
 
 runid="$1"
 config="$2"
+shift 2
 
 for i in $(seq 1 50); do
-    pipenv run python src/main.py --task=$i --config="$config" --runid="$runid" --train --test
+    pipenv run python src/main.py --task=$i --config="$config" --prev-id="$runid" --train --test "$@"
     runid=$(cat .runid)
 done 
